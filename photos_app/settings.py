b2c58f2532abba42cdaf11b65752d17c3c5bf9d0
yahani <yahanihewagama@yahoo.com>
2020-02-27 02:54:20 +0800

    remove unnecessary code

--- photos_app/settings.py
+++ photos_app/settings.py
@@ -113,11 +113,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
